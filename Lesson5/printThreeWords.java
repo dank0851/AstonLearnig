5393944744d21d1de8fa400ed9be6e21b50b7575
dank0851 <dan_k0@mail.ru>
2024-06-28 22:58:30 +0300

    Create printThreeWords.java

---
+++ Lesson5/printThreeWords.java
@@ -0,0 +1,10 @@
+public class printThreeWords{ 
+    // Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple./  
+    public static void main (String args[]){
+          
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
+
